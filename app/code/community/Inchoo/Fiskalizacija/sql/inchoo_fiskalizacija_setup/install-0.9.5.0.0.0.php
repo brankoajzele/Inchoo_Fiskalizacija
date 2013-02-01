332876fd1ce1bc22c0fb5d2a51aa6c46e35d5d8e
Branko Ajzele <ajzele@gmail.com>
2013-02-01 20:00:50 +0100

    Preparing v0.9.5.0.0.0 for Magento Connect.

--- app/code/community/Inchoo/Fiskalizacija/sql/inchoo_fiskalizacija_setup/install-0.9.5.0.0.0.php
+++ app/code/community/Inchoo/Fiskalizacija/sql/inchoo_fiskalizacija_setup/install-0.9.5.0.0.0.php
@@ -36,13 +36,6 @@ $installer = $this;
 
 $installer->startSetup();
 
-/* START Cleanup, full remove */
-$installer->run("
-DROP TABLE IF EXISTS {$installer->getTable('inchoo_fiskalizacija/cert')};
-DROP TABLE IF EXISTS {$installer->getTable('inchoo_fiskalizacija/invoice')};
-");
-/* END Cleanup, full remove */
-
 $table = $installer->getConnection()
     ->newTable($installer->getTable('inchoo_fiskalizacija/cert'))
     ->addColumn('cert_id', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array(
